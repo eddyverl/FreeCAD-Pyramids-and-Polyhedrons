deb7a26f521d4e44d2a1551fb6eedf89513dd772
Eddy Verlinden <eddyverl@users.noreply.github.com>
2019-05-08 15:13:34 +0200

    Update pyramid_as_part.py

--- pyramid_as_part.py
+++ pyramid_as_part.py
@@ -1,109 +1,68 @@
-#   Based on an early version of pyramidenstumpf.FCMacro
-#   Added: real pyramids (with a point on top/bottom)
-#   Added: an input form to enter the data of the pyramid
-
-#*****#**********************************************************************
-#*                                                                         *
-#*   Copyright (c) 2014                                                    *  
-#*   Thomas Gundermann <thomas@freecadbuch.de>                             * 
-#*                                                                         *
-#*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
-#*   as published by the Free Software Foundation; either version 2 of     *
-#*   the License, or (at your option) any later version.                   *
-#*   for detail see the LICENCE text file.                                 *
-#*                                                                         *
-#*   This program is distributed in the hope that it will be useful,       *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Library General Public License for more details.                  *
-#*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
-#*   License along with this program; if not, write to the Free Software   *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-#*   USA                                                                   *
-#*                                                                         *
-#***************************************************************************
-
-
+"""
+author: Eddy Verlinden, Genk Belgium
+licence : MIT
+"""
 
 import math
-from math import sqrt, pi, sin, cos, asin
-
 import sys
 from PySide import QtGui, QtCore
 
 
-def say(s):
+
+def horizontal_regular_polygon_vertexes(sidescount,radius,z):
+
+    vertexes = []
+    if radius != 0 :
+        for i in range(0,sidescount+1):
+            angle = 2 * math.pi * i / sidescount
+            vertex = (radius * math.cos(angle), radius * math.sin(angle), z)
+            vertexes.append(vertex)
+    else:
+        vertex = (0,0,z)
+        vertexes.append(vertex)
+
+    return vertexes
+
+        
+def build_pyramid(sidescount,radius_bottom, radius_top, height):
+    
+    faces = []
+    if radius_bottom != 0 or radius_top != 0:
+
+        vertexes_bottom = horizontal_regular_polygon_vertexes(sidescount,radius_bottom,0)
+        vertexes_top = horizontal_regular_polygon_vertexes(sidescount,radius_top,height)
+
+        if radius_bottom != 0:
+            polygon_bottom = Part.makePolygon(vertexes_bottom)
+            face_bottom = Part.Face(polygon_bottom)
+            faces.append(face_bottom)
+        if radius_top != 0:    
+            polygon_top = Part.makePolygon(vertexes_top)
+            face_top = Part.Face(polygon_top)
+            faces.append(face_top)
+
+        for i in range(sidescount):            
+            if radius_top == 0:
+                vertexes_side=[vertexes_bottom[i],vertexes_bottom[i+1],vertexes_top[0],vertexes_bottom[i]]
+            elif radius_bottom == 0:
+                vertexes_side=[vertexes_bottom[0],vertexes_top[i+1],vertexes_top[i],vertexes_bottom[0]]
+            else:
+                vertexes_side=[vertexes_bottom[i],vertexes_bottom[i+1],vertexes_top[i+1],vertexes_top[i],vertexes_bottom[i]]
+            polygon_side=Part.makePolygon(vertexes_side)
+            faces.append(Part.Face(polygon_side))
+
+        shell = Part.makeShell(faces)   
+        solid = Part.makeSolid(shell)
+        obj=App.ActiveDocument.addObject("Part::Feature","Pyramid")
+        obj.Shape = solid
+
+def msgbox(s):
 	msg = QtGui.QMessageBox()
 	msg.setIcon(QtGui.QMessageBox.Information)
 	msg.setText(s)
 	msg.setWindowTitle("Message")
 	msg.setStandardButtons(QtGui.QMessageBox.Ok )	
 	retval = msg.exec_()
-
-
-def vieleck(anz,size,hoehe): # regelmaesiges vieleck berechnen
-	list1=[]
-	for p in range(anz):
-		if size != 0 :
-			punkt=( size*cos(2*math.pi *p/anz),size*sin(2*math.pi*p/anz),hoehe)
-		else:
-			punkt= (0,0,hoehe)
-		list1.append(punkt)
-		#say(punkt)
-	
-	p=0
-	if size != 0 :
-		punkt=( size*cos(2*math.pi *p/anz),size*sin(2*math.pi*p/anz),hoehe)
-	else:
-		punkt= (0,0,hoehe)
-	list1.append(punkt)
-	#say(list1)
-	return list1
-
-
-def gen_pyramidenstumpf(count=8,size_bottom = 60, size_top=20, height=60):
-	if size_bottom != 0 or size_top !=0 : 
-		list1=vieleck(count,size_bottom,0)
-		list2=vieleck(count,size_top,height)
-		
-		if size_bottom != 0 and size_top != 0:
-			poly1 = Part.makePolygon( list1)
-			face1 = Part.Face(poly1)
-			poly2 = Part.makePolygon( list2)
-			face2 = Part.Face(poly2)
-			faceListe=[face1,face2]
-		elif size_top != 0:
-			poly2 = Part.makePolygon( list2)
-			face2 = Part.Face(poly2)
-			faceListe=[face2]
-		else:    
-			poly1 = Part.makePolygon( list1)
-			face1 = Part.Face(poly1)
-			faceListe=[face1]
-		
-		for i in range(len(list1)-1):
-			if size_top == 0:
-				liste3=[list1[i],list1[i+1],list2[i],list1[i]]
-			elif size_bottom == 0:
-				liste3=[list1[i],list2[i+1],list2[i],list1[i]]
-			else:
-				liste3=[list1[i],list1[i+1],list2[i+1],list2[i],list1[i]]
-
-			poly=Part.makePolygon(liste3)
-			face = Part.Face(poly)
-			faceListe.append(face)
-			#say(i);say(poly);say(faceListe)
-		
-		myShell = Part.makeShell(faceListe)   
-		mySolid = Part.makeSolid(myShell)
-
-		obj=App.ActiveDocument.addObject("Part::Feature","Pymf")
-		obj.Shape = mySolid
-
-#gen_pyramidenstumpf(5,50,25,20)   #testexample
-
 
 
 
@@ -192,7 +151,7 @@ class PyramidDialog(QtGui.QWidget):
 			sidet = float(str(self.sidet.text()))
 
 		if (n < 3) or ( radiusb == 0 and radiust == 0 and sideb == 0 and sidet == 0):
-			say("INPUT ERROR!")
+			msgbox("INPUT ERROR!")
 
 		else :
 			angle_center = 2 * math.pi / n
@@ -201,7 +160,7 @@ class PyramidDialog(QtGui.QWidget):
 			if sidet != 0 :
 				radiust = (sidet/2) / cos ((math.pi - angle_center) / 2)
 	
-			gen_pyramidenstumpf(n,radiusb,radiust,height)
+			build_pyramid(n,radiusb,radiust,height)
 			self.close()
 
 	def cancel_method(self):
