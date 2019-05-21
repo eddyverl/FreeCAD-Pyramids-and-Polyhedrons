27fa3dbed3d16784a8ae066e151c397469d59724
Eddy Verlinden <eddyverl@users.noreply.github.com>
2019-05-21 14:15:55 +0200

    Update pyramid_as_part.py

--- pyramid_as_part.py
+++ pyramid_as_part.py
@@ -156,9 +156,9 @@ class PyramidDialog(QtGui.QWidget):
 		else :
 			angle_center = 2 * math.pi / n
 			if sideb != 0 :
-				radiusb = (sideb/2) / cos ((math.pi - angle_center) / 2)
+				radiusb = (sideb/2) / math.cos ((math.pi - angle_center) / 2)
 			if sidet != 0 :
-				radiust = (sidet/2) / cos ((math.pi - angle_center) / 2)
+				radiust = (sidet/2) / math.cos ((math.pi - angle_center) / 2)
 	
 			build_pyramid(n,radiusb,radiust,height)
 			self.close()
