d0d393d4faabc8b6f8cfb6335a3b539997beb620
hasecilu <hasecilu@tuta.io>
2024-06-04 12:13:26 -0600

    Add Spanish translations

---
+++ Resources/Translations/PandP_es-ES.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="en">
+<context>
+    <name>Commands</name>
+    <message>
+        <location filename="../../polyhedrons.py" line="256"/>
+        <source>Pyramid</source>
+        <translation>Pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="257"/>
+        <source>Generate a Pyramid with any number of sides</source>
+        <translation>Generar una pirámide con cualquier número de lados</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="328"/>
+        <source>Tetrahedron</source>
+        <translation>Tetraedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="329"/>
+        <source>Generate a Tetrahedron</source>
+        <translation>Generar un tetraedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="398"/>
+        <source>Hexahedron</source>
+        <translation>Hexaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="399"/>
+        <source>Generate a Hexahedron</source>
+        <translation>Generar un hexaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="467"/>
+        <source>Octahedron</source>
+        <translation>Octaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="468"/>
+        <source>Generate a Octahedron</source>
+        <translation>Generar un octaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="564"/>
+        <source>Dodecahedron</source>
+        <translation>Dodecaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="565"/>
+        <source>Generate a Dodecahedron</source>
+        <translation>Generar un dodecaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="655"/>
+        <source>Icosahedron</source>
+        <translation>Icosaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="656"/>
+        <source>Generate a Icosahedron</source>
+        <translation>Generar un icosaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="814"/>
+        <source>Icosahedron truncated</source>
+        <translation>Icosaedro truncado</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="815"/>
+        <source>Generate a Truncated Icosahedron (football)</source>
+        <translation>Generar un icosaedro truncado (balón de fútbol)</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="977"/>
+        <source>Geodesic sphere</source>
+        <translation>Esfera geodésica</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="978"/>
+        <source>Generate Geodesic Spheres</source>
+        <translation>Generar una esfera geodésica</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1450"/>
+        <source>Regular Solid</source>
+        <translation>Sólido regular</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1451"/>
+        <source>Generate a Regular Solid</source>
+        <translation>Generar un sólido regular</translation>
+    </message>
+</context>
+<context>
+    <name>P&amp;P</name>
+    <message>
+        <location filename="../../InitGui.py" line="60"/>
+        <source>Pyramids-and-Polyhedrons</source>
+        <translation>Pirámides y poliedros</translation>
+    </message>
+    <message>
+        <location filename="../../InitGui.py" line="61"/>
+        <source>A workbench for generating pyramids, polyhedrons and geodesic spheres</source>
+        <translation>Un entorno de trabajo para generar pirámides, poliedros y esferas geodésicas</translation>
+    </message>
+</context>
+<context>
+    <name>Properties tooltips</name>
+    <message>
+        <location filename="../../polyhedrons.py" line="167"/>
+        <location filename="../../polyhedrons.py" line="161"/>
+        <source>Radius of the pyramid</source>
+        <translation>Radio de la pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="173"/>
+        <source>Height of the pyramid</source>
+        <translation>Altura de la pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="179"/>
+        <source>Sidescount of the pyramid</source>
+        <translation>Número de lados de la pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="185"/>
+        <source>Sidelength1 of the pyramid</source>
+        <translation>Longitud del lado 1 de la pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="191"/>
+        <source>Sidelength2 of the pyramid</source>
+        <translation>Longitud del lado 2 de la pirámide</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="197"/>
+        <source>alfa angle around Z</source>
+        <translation>Ángulo alfa alrededor de Z</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="332"/>
+        <source>Radius of the tetrahedron</source>
+        <translation>Radio del tetraedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="338"/>
+        <source>Sidelength of the tetrahedron</source>
+        <translation>Longitud del lado del tetraedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="414"/>
+        <source>Radius of the hexahedron</source>
+        <translation>Radio del hexaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="420"/>
+        <source>Sidelength of the hexahedron</source>
+        <translation>Longitud del lado del hexaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="504"/>
+        <source>Radius of the octahedron</source>
+        <translation>Radio del octaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="510"/>
+        <source>Sidelength of the octahedron</source>
+        <translation>Longitud del lado del octaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="592"/>
+        <source>Radius of the dodecahedron</source>
+        <translation>Radio del dodecaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="598"/>
+        <source>Sidelength of the dodecahedron</source>
+        <translation>Longitud del lado del dodecaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="713"/>
+        <source>Radius of the icosahedron</source>
+        <translation>Radio del icosaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="719"/>
+        <source>Sidelength of the icosahedron</source>
+        <translation>Longitud del lado del icosaedro</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="822"/>
+        <source>Radius</source>
+        <translation>Radio</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="828"/>
+        <source>Sidelength</source>
+        <translation>Longitud del lado</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1098"/>
+        <source>Radius of the sphere</source>
+        <translation>Radio de la esfera</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1104"/>
+        <source>Sidelength of the triangles (approximative!)</source>
+        <translation>Longitud del lado de los triángulos (¡aproximación!)</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1110"/>
+        <source>The sides of the basic polyhedron are divided in ... (value 1 to 10)</source>
+        <translation>Los lados del poliedro básico son divididos en ... (valor del 1 al 10)</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1730"/>
+        <source>Radius of inscribed sphere touching closest edge</source>
+        <translation>Radio de la esfera inscrita tocando a la arista más cercana</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1736"/>
+        <source>Radius of inscribed sphere touching closest face</source>
+        <translation>Radio de la esfera inscrita tocando a la cara más cercana</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1742"/>
+        <source>Radius of inscribed sphere touching furthest vertex</source>
+        <translation>Radio de la esfera inscrita tocando al vértice más lejano</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1748"/>
+        <source>Length of longest edge</source>
+        <translation>Longitud de la arista más larga</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1754"/>
+        <source>Length of shortest edge</source>
+        <translation>Longitud de la arista más corta</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1760"/>
+        <source>What drives solid size when changing construction</source>
+        <translation>Lo que controla el tamaño del sólido al cambiar de construcción</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1768"/>
+        <source>Initiating body</source>
+        <translation>Cuerpo inicial</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1776"/>
+        <source>Amount of vertex truncation/elongation</source>
+        <translation>Cantidad de truncamiento/elongación de vértices</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1782"/>
+        <source>Amount of edge truncation</source>
+        <translation>Cantidad de truncamiento de aristas</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1788"/>
+        <source>Create the snub version</source>
+        <translation>Crear la versión achatada</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1796"/>
+        <source>Create the dual of the current solid</source>
+        <translation>Crear el dual del sólido actual</translation>
+    </message>
+    <message>
+        <location filename="../../polyhedrons.py" line="1802"/>
+        <source>Preset parameters for some hard names</source>
+        <translation>Parámetros preconfigurados para algunos sólidos</translation>
+    </message>
+</context>
+</TS>
